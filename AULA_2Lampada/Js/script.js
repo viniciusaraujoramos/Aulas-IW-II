9d45f007feeab5593068cac590af83bfc79a6d9d
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-02-24 17:22:49 -0300

    Add files via upload

--- AULA_2Lampada/Js/script.js
+++ AULA_2Lampada/Js/script.js
@@ -1,25 +1,52 @@
 const ligar = document.getElementById ('ligar')
 const desligar = document.getElementById ('desligar')
+const ambos = document.getElementById ('ambos')
 const lamp = document.getElementById ('lamp')
+const estado = document.getElementById ('estado')
 
 function estaQuebrada(){
     return lamp.src.indexOf('quebrada') > -1
+}
+
+function comoEsta(){
+    return lamp.src.indexOf('desligada') > -1 
+}
+
+function comoEsta2(){
+    return lamp.src.indexOf('ligada') > -1 
 }
 
 function acender (){
     if(!estaQuebrada()){
         lamp.src = "img/ligada.jpg"
     }
+        if(comoEsta2()){           
+            estado.innerHTML = "A lâmpada está LIGADA"   
+        }
 }
 
 function apagar (){
     if(!estaQuebrada()){
         lamp.src = "img/desligada.jpg"
     }
+        if(comoEsta()){
+            estado.innerHTML = "A lâmpada está DESLIGADA"
+        }
 }
 
 function quebrar (){
     lamp.src = "img/quebrada.jpg"
+    estado.innerHTML = "A lâmpada está QUEBRADA"
+}
+
+function mult (){
+    if(comoEsta()){
+        acender()
+    }
+    else{
+        apagar()
+    }
+    
 }
 
 ligar.addEventListener('click',acender)
@@ -28,3 +55,5 @@ desligar.addEventListener('click',apagar)
 lamp.addEventListener('mouseover',acender)
 lamp.addEventListener('mouseleave',apagar)
 lamp.addEventListener('dblclick',quebrar)
+
+ambos.addEventListener('click', mult)

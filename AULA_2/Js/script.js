f1968b20cd225d813458abea8ced471ac098eba4
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2024-03-04 19:28:06 -0300

    Nomes

---
+++ AULA_2/Js/script.js
@@ -0,0 +1,66 @@
+const ligar = document.getElementById ('ligar')
+const desligar = document.getElementById ('desligar')
+const ambos = document.getElementById ('ambos')
+const reset = document.getElementById ('reset')
+const lamp = document.getElementById ('lamp')
+const estado = document.getElementById ('estado')
+
+function estaQuebrada(){
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function comoEsta(){
+    return lamp.src.indexOf('desligada') > -1 
+}
+
+function comoEsta2(){
+    return lamp.src.indexOf('ligada') > -1 
+}
+
+function acender (){
+    if(!estaQuebrada()){
+        lamp.src = "img/ligada.jpg"
+    }
+        if(comoEsta2()){           
+            estado.innerHTML = "A lâmpada está LIGADA"   
+        }
+}
+
+function apagar (){
+    if(!estaQuebrada()){
+        lamp.src = "img/desligada.jpg"
+    }
+        if(comoEsta()){
+            estado.innerHTML = "A lâmpada está DESLIGADA"
+        }
+}
+
+function quebrar (){
+    lamp.src = "img/quebrada.jpg"
+    estado.innerHTML = "A lâmpada está QUEBRADA"
+}
+
+function mult (){
+    if(comoEsta()){
+        acender()
+    }
+    else{
+        apagar()
+    }
+    
+}
+
+function resetar (){
+    lamp.src = "img/desligada.jpg"
+    estado.innerHTML = "A lâmpada esá DESLIGADA"
+}
+
+ligar.addEventListener('click',acender)
+desligar.addEventListener('click',apagar)
+
+lamp.addEventListener('mouseover',acender)
+lamp.addEventListener('mouseleave',apagar)
+lamp.addEventListener('dblclick',quebrar)
+
+ambos.addEventListener('click', mult)
+reset.addEventListener('click',resetar)

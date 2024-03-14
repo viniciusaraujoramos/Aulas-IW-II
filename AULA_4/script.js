a2d09c5916d5233cea7e18dbf255b41f574f93e2
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2024-03-13 23:02:16 -0300

    sei la

--- AULA_4/script.js
+++ AULA_4/script.js
@@ -4,4 +4,47 @@ function calcular(){
     var imc = peso/(altura*altura)
 
     resultado.innerHTML = imc
+    diag(calcular)
+}
+
+function diag(calcular){
+    var sexo = document.getElementById('sexo').value
+    if(sexo=='mas'){
+        if (imc < 20){
+            diagnostico.innerHTML = "Abaixo do peso"
+        }
+        else if (19 > imc && imc < 24){
+            diagnostico.innerHTML = "Normal"
+        }
+        else if (25 > imc && imc < 29){
+            diagnostico.innerHTML = "Obesidade leve"
+        }
+        else if (30 > imc && imc < 39){
+            diagnostico.innerHTML = "Obesidade moderada"
+        }
+        else{
+            diagnostico.innerHTML = "Obesidade mórbida"
+        }
+    }
+    else if(sexo=='fem'){
+        if (imc < 19){
+            diagnostico.innerHTML = "Abaixo do peso"
+        }
+        else if (19 > imc && imc < 23){
+            diagnostico.innerHTML = "Normal"
+        }
+        else if (24 > imc && imc < 28){
+            diagnostico.innerHTML = "Obesidade leve"
+        }
+        else if (29 > imc && imc< 38){
+            diagnostico.innerHTML = "Obesidade moderada"
+        }
+        else{
+            diagnostico.innerHTML = "Obesidade mórbida"
+        }
+    }
+    else{
+        diagnostico.innerHTML = 'Erro de dados'
+    }
+    
 }

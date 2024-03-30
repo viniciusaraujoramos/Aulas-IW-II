02cf76df533641cde9d388a72548f1ac29a5e5db
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2024-03-30 14:51:53 -0300

    gif

---
+++ AULA_5/ManipulandoCSS/script.js
@@ -0,0 +1,43 @@
+let titulo = document.querySelector('h1')
+let tela = document.querySelector('main')
+let BE = document.querySelector('#E')
+let BR = document.querySelector('#R')
+let BG = document.querySelector('#G')
+let BS = document.querySelector('#S')
+let BI = document.querySelector('#I')
+let BH = document.querySelector('#H')
+let imagem = document.querySelector('#gif')
+let NTecnica = document.querySelector('#Ntecnica')
+let Personagem = document.querySelector('#Personagem')
+let Info = document.querySelector('#Info')
+
+BE.addEventListener('click', Endo)
+BR.addEventListener('click', Ronaldinho)
+BG.addEventListener('click', Goenji)
+BS.addEventListener('click', Someoca)
+BI.addEventListener('click', Intituto)
+BH.addEventListener('click', Helio)
+
+function Endo() {
+    imagem.setAttribute('src', 'img/Mao.gif')
+}
+
+function Ronaldinho(){
+
+}
+
+function Goenji(){
+    
+}
+
+function Someoca(){
+    
+}
+
+function Intituto(){
+
+}
+
+function Helio(){
+    
+}

ce45155dc797343550e2c209d7cb12b7ecfe887f
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-04-25 09:05:50 -0300

    Add files via upload

---
+++ DesafioMario/scrip.js
@@ -0,0 +1,12 @@
+/*Definindo as constantes*/ 
+const mario = document.querySelector(".mario");
+
+const jump=()=>{
+    mario.classList.add('jump');
+
+    setTimeout(()=>{
+        mario.classList.remove('jump');
+    },500);
+}
+
+document.addEventListener('keydown', jump);
